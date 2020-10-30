a3b9cb53c0161d36fd13c73e32de84eb427db4f3
Alejandro Espinoza <alehernandz99@gmail.com>
2020-10-29 21:43:10 -0600

    get the array.reverse of the user number

--- script.js
+++ script.js
@@ -14,4 +14,23 @@ function getData() {
     }
     console.log(arrayNumberCard);
 } 
+
+function reversedData(){
+    //obtengo la reversa del numero de tarjeta que introdujo el usuario
+    arrayReversed = arrayNumberCard.reverse();
+    console.log(arrayReversed);
+}
+
+function luhnAlgorithm() {
+    for(i = 0; i < arrayReversed.length; i++){
+        //Verifico si es posicion par o no, si es, lo multiplico por 2
+        if(i%2 == 0){ 
+            arrayNumberCard[i] = arrayReversed[i] * 2;
+        }
+    }
+    console.log(arrayNumberCard);
+}
+
 sendData.addEventListener("click", getData);
+sendData.addEventListener("click", reversedData);
+sendData.addEventListener("click", luhnAlgorithm);
