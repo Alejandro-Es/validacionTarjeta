1ffaf00bcb064238eeb1fea138825d14ed1f8ec3
Alejandro Espinoza <alehernandz99@gmail.com>
2020-11-20 17:36:00 -0600

    I add each digit of the numbers greater than 10

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
 const inputCard = document.getElementById("inputCard");
 const sendData = document.getElementById("sendData");
 let cardNumber = []
-let arrayNumberCard = [];
+let arrayCard = [];
 let arrayReversed;
+let arrayCardToString;
 
-function getData() {
+function luhnAlgorithm() {
     //Obtengo el numero de la tarjeta del usuario y la almaceno en un array
     // cardNumber = Array.from(inputCard.value);
     // console.log(cardNumber);
     cardNumber = inputCard.value;
     for (i = 0; i < cardNumber.length; i++){ 
-        arrayNumberCard[i] = cardNumber[i];
+        arrayCard[i] = cardNumber[i];
     }
-    console.log(arrayNumberCard);
-} 
 
-function reversedData(){
     //obtengo la reversa del numero de tarjeta que introdujo el usuario
-    arrayReversed = arrayNumberCard.reverse();
-    console.log(arrayReversed);
-}
+    arrayReversed = arrayCard.reverse();
 
-function luhnAlgorithm() {
     for(i = 0; i < arrayReversed.length; i++){
         //Verifico si es posicion par o no, si es, lo multiplico por 2
-        if(i%2 == 0){ 
-            arrayNumberCard[i] = arrayReversed[i] * 2;
+        if(i%2 !== 0){ 
+            arrayCard[i] = arrayReversed[i] * 2;
         }
     }
-    console.log(arrayNumberCard);
+    console.log(arrayCard);
+    //luego sumo los digitos de los numeros mayores a 10
+    arrayCardToString = arrayCard.toString();
+    arrayCard = arrayCardToString.split(",")
+    console.log(arrayCard)
+    for (i = 0; i < arrayCard.length; i++){
+        if(arrayCard[i].length === 2){ 
+            arrayCard[i] = parseInt(arrayCard[i][0]) + parseInt(arrayCard[i][1]);
+        } else {
+            arrayCard[i] = parseInt(arrayCard[i]);
+        }
+    }
 }
 
-sendData.addEventListener("click", getData);
-sendData.addEventListener("click", reversedData);
 sendData.addEventListener("click", luhnAlgorithm);
