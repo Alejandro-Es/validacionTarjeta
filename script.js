575be72136e426d95fe9411b1602c56ebb0e29e9
Alejandro Espinoza <alehernandz99@gmail.com>
2020-11-20 22:29:14 -0600

    I add all the digits and verify if it is valid or not

--- script.js
+++ script.js
@@ -13,10 +13,10 @@ function luhnAlgorithm() {
     for (i = 0; i < cardNumber.length; i++){ 
         arrayCard[i] = cardNumber[i];
     }
-
+    console.log(cardNumber);
     //obtengo la reversa del numero de tarjeta que introdujo el usuario
     arrayReversed = arrayCard.reverse();
-
+    console.log(arrayReversed);
     for(i = 0; i < arrayReversed.length; i++){
         //Verifico si es posicion par o no, si es, lo multiplico por 2
         if(i%2 !== 0){ 
@@ -27,14 +27,28 @@ function luhnAlgorithm() {
     //luego sumo los digitos de los numeros mayores a 10
     arrayCardToString = arrayCard.toString();
     arrayCard = arrayCardToString.split(",")
-    console.log(arrayCard)
+
     for (i = 0; i < arrayCard.length; i++){
         if(arrayCard[i].length === 2){ 
             arrayCard[i] = parseInt(arrayCard[i][0]) + parseInt(arrayCard[i][1]);
         } else {
             arrayCard[i] = parseInt(arrayCard[i]);
         }
+        
     }
+    console.log(arrayCard)
+    //Sumo todos los digitos y si es multiplo de 10 es valida
+    let sumArrayCard = 0;
+    for(i = 0; i < arrayCard.length; i++){
+        sumArrayCard += arrayCard[i];
+    }
+    console.log(sumArrayCard);
+    if(sumArrayCard %10 == 0 ) {
+        alert("Su tarjeta es valida");
+    } else {
+        alert("Su tarjeta no es valida");
+    }
+
 }
 
 sendData.addEventListener("click", luhnAlgorithm);
